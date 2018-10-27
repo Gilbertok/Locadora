cbf08d74f7d1ffd9127e2723271ba0ccb5d1811c
Gilberto kolacki <gilbertokolacki@gmail.com>
2018-10-26 23:03:41 -0300

    melhorias

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,15 @@ $('.carousel').carousel({
 
 $('.modal').on('hidden.bs.modal', function () {
     video = $(this).closest("div").find('video')[0];
-    video.pause();
-    video.currentTime = 0;
+    if(video != undefined) {
+        video.pause();
+        video.currentTime = 0;
+    }
 })
 
 $('.modal').on('shown.bs.modal', function () {
-    $(this).closest("div").find('video')[0].play();
+    video = $(this).closest("div").find('video')[0];
+    if(video != undefined) {
+        video.play();
+    }
 })
