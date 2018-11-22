bf8cf13bf1fe95b99e566ed317951b0c27b0515e
Leonardo Mees <leonardomees95@gmail.com>
2018-11-22 20:24:58 -0200

    edit pg aventura e terror

---
+++ src/pages/aventura.js
@@ -0,0 +1,77 @@
+var filmes = [
+    {
+        titulo: 'Vingadores: Guerra do Infinito',
+        conteudo: 'Aventura, Ação',
+        dataLancamento: '26 de abril de 2018 (2h 36min)',
+        direcao: 'Joe Russo, Anthony Russo',
+        sinopse: 'Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.',
+        linkImagem: 'Aventura/aventuravingadores',
+        dataTarget: '',
+        linkFilme: ''
+    },{
+        titulo: 'COMO TREINAR O SEU DRAGÃO 2',
+        conteudo: 'Animação, Aventura, Família',
+        dataLancamento: '19 de junho de 2014 (1h 43min)',
+        direcao: 'Dean DeBlois',
+        sinopse: 'Cinco anos se passaram desde que Soluço estabeleceu a paz com os dragões e vive em harmonia, na Ilha de Berk, com Banguela. Eles voam, apostam corridas e se divertem muito. Em uma destas aventuras, descobrem uma caverna secreta cheia de dragões. Agora, a dupla luta para proteger Berk de um guerreiro perigoso, chamado Drago Bludvist, que deseja controlar todos os dragões existentes.',
+        linkImagem: 'Aventura/comotreinardragao.jpg',
+        dataTarget: '',
+        linkFilme: ''
+    },{
+        titulo: 'HAN SOLO: UMA HISTÓRIA STAR WARS',
+        conteudo: 'Aventura, Ação',
+        dataLancamento: '24 de maio de 2018 (2h 15min)',
+        direcao: 'Ron Howard',
+        sinopse: 'Em dificuldade financeira, o piloto Han Solo (Alden Ehrenreich) e sua namorada Qira (Emilia Clarke) buscam algum trabalho. Eles conseguem pôr as mãos em uma substância valiosa, mas perigosa, que acaba levando à perseguição do Império e à separação entre os dois. Preocupado, Han usa toda a sua esperteza para se infiltrar num plano arriscado, mas que pode render a oportunidade de voltar à terra natal e reencontrar a sua amada. Caso ele falhe, vai ter que prestar contas ao perigoso Dryden Vos (Paul Bettany). No caminho, o piloto encontra parceiros importantes, como Chewbacca (Joonas Suotamo) e Lando Calrissian (Donald Glover), o proprietário original da Millennium Falcon.',
+        linkImagem: 'Aventura/hansolo.jpg',
+        dataTarget: '',
+        linkFilme: ''
+    },{
+        titulo: 'MOANA - UM MAR DE AVENTURAS',
+        conteudo: 'Animação, Aventura, Família',
+        dataLancamento: '5 de janeiro de 2017 (1h 47min)',
+        direcao: 'John Musker, Ron Clements',
+        sinopse: 'Moana Waialiki é uma corajosa jovem, filha do chefe de uma tribo na Oceania, vinda de uma longa linhagem de navegadores. Querendo descobrir mais sobre seu passado e ajudar a família, ela resolve partir em busca de seus ancestrais, habitantes de uma ilha mítica que ninguém sabe onde é. Acompanhada pelo lendário semideus Maui, Moana começa sua jornada em mar aberto, onde enfrenta terríveis criaturas marinhas e descobre histórias do submundo.',
+        linkImagem: '',
+        dataTarget: '',
+        linkFilme: ''
+    },{
+        titulo: 'DUPLA EXPLOSIVA ',
+        conteudo: 'Ação',
+        dataLancamento: '31 de agosto de 2017 (1h 58min)',
+        direcao: 'Patrick Hughes (II)',
+        sinopse: 'O principal guarda-costas do mundo possui um novo cliente: um assassino de aluguel que precisa testemunhar na Corte Internacional de Justiça. Por anos eles estavam em lados opostos de um tiro, mas agora eles estão presos juntos. Eles precisam colocar as diferenças de lado para chegarem ao julgamento a tempo.',
+        linkImagem: 'Acao/duplaexplosiva.jpg',
+        dataTarget: 'trailer_dupla_explosiva',
+        linkFilme: ''
+    },{
+        titulo: 'ALEM DA ESCURIDÃO',
+        conteudo: 'Ação, Ficção científica',
+        dataLancamento: '2012 (1h 36min)',
+        direcao: 'Martin Guigui',
+        sinopse: 'Em uma cidade pequena onde todos se conhecem, há rumores de que a casa de um dos moradores é assombrada. Quando um grupo de jovens decide investigar, são surpreendidos por Ely (Dennis Quaid), o proprietário, que mata um deles. Logo, os outros tentam provar o crime mas como Ely é um cidadão respeitado na cidade, ninguém acredita neles e o homem passa a persegui-los. Como eles poderão revelar que há um psicopata à espreita? ',
+        linkImagem: 'Acao/escuridao',
+        dataTarget: 'trailer_alem_da_escuridao',
+        linkFilme: ''
+    },{
+        titulo: 'THE FOREIGNER',
+        conteudo: 'Ação',
+        dataLancamento: '11 de janeiro de 2018 (1h 54min)',
+        direcao: 'Martin Campbell',
+        sinopse: 'Quan (Jackie Chan) é dono de um típico restaurantes chinês em Londres, capital da Inglaterra. Após um misterioso ataque de um grupo de terroristas irlandeses ao seu estabelecimento, ele tem sua vida e família devastadas. Sem muito apoio da polícia local, ele vai buscar vingança com suas próprias mãos.',
+        linkImagem: 'Acao/foreigner',
+        dataTarget: 'trailer_the_forigner',
+        linkFilme: ''
+    },{
+        titulo: 'LINHA DE AÇÃO',
+        conteudo: 'Ação, Policial, Suspense',
+        dataLancamento: '15 de março de 2013 (1h 49min))',
+        direcao: 'Allen Hughes',
+        sinopse: 'Um ex-policial é envolvido em um escândalo após ser chamado pelo prefeito de Nova York para seguir a esposa do político que desconfia de infidelidade da mulher. Ao perceber que foi enganado, o detetive sai em busca de justiça.',
+        linkImagem: 'Acao/linhaacao',
+        dataTarget: 'trailer_linha_de_acao',
+        linkFilme: ''
+    }
+];
+
+window.onload = carregarFilmes(filmes);
